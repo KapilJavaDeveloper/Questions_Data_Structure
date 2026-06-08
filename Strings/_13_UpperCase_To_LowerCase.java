6cdf34e0c14ebb76ba5f459adf2a86bf4bdffa4b
Kapil <kk7094948@gmail.com>
2026-06-08 12:02:58 +0530

    UpperCase To LowerCase

---
+++ Strings/_13_UpperCase_To_LowerCase.java
@@ -0,0 +1,16 @@
+public class _13_UpperCase_To_LowerCase {
+    public static void main(String [] args){
+        System.out.println("UpperCase to LowerCase String Program") ;
+
+        String str = "HeLLO WORLD" ;
+
+        System.out.println("String be: "+ str) ;
+
+        String lowerString = str.toLowerCase() ;
+
+        System.out.println("Lower Case String be: "+ lowerString) ;
+
+
+    }
+    
+}
